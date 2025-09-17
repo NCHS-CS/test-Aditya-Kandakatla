f07f3782a6934be5117d57ece462eb1e2a05e024
Aditya-Kandakatla <2002212@apps.nsd.org>
2025-09-17 15:46:30 +0000

    Done

--- src/main/java/org/nhs/app/App.java
+++ src/main/java/org/nhs/app/App.java
@@ -1,6 +1,6 @@
 class App {
-public static void main(String args[]){
-
-    System.out.print("Hello world");
+    public static void main(String args[])
+    {
+        System.out.print("Hello world");
 }
 }
