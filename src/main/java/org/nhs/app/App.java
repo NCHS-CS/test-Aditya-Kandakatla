9d00dde97e14958b4a1c40eb4d81cefb17479146
Aditya-Kandakatla <2002212@apps.nsd.org>
2025-09-17 15:42:52 +0000

    hhhhehe

--- src/main/java/org/nhs/app/App.java
+++ src/main/java/org/nhs/app/App.java
@@ -1,8 +1,6 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+class App {
+public static void main(String args[]){
 
-package org.nhs.app;
-
-// Your code goes here
+    System.out.print("Hello world");
+}
+}
